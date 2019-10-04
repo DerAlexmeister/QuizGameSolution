16e2ab44f2af079a135e193037cba30fc0cfe2ed
DerAlexx <alexweb199@hotmail.de>
2019-10-04 19:04:02 +0200

    Added a Timer and some smaller supporting functions and smilies 😁 😂

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
-/**
-* This repo contains my solution to the exercise
-* from https://gophercises.com
- */
+/*
+This repo contains my solution to the exercise
+from https://gophercises.com
+@Author DerAlexmeister
+*/
 
 package main
 
@@ -14,6 +15,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 /*
@@ -22,6 +24,14 @@ CSVPair represents a pair of question and answer
 type CSVPair struct {
 	Question string
 	Answer   int64
+}
+
+/*
+Game represents generalinformation about your game
+*/
+type Game struct {
+	CorrectAnswers int
+	TimeOutAfter   int
 }
 
 /*
@@ -63,14 +73,25 @@ func readCSV(file *os.File) []CSVPair {
 }
 
 /*
+timer is a timer that will wait a given time in Milliseconds
+and in case the goroutine is not suspended it will exit
+*/
+func timer(timeout int) {
+	time.Sleep(time.Millisecond * time.Duration(timeout))
+	fmt.Printf("❔ \n >> You run out of time 😨 << ")
+	os.Exit(0)
+}
+
+/*
 AskAQuestion will print a Mathproblem
 Then what for the User to answer the Question
 After the Users answers it will return true or false in case the given
 Answer was correct or not
 */
-func askAQuestion(questionNumber int, pair CSVPair) bool {
+func askAQuestion(questionNumber int, pair CSVPair, time int) bool {
 	var UserAnswer int64
-	fmt.Printf("Question #%d: %s = ", questionNumber, pair.Question)
+	go timer(time)
+	fmt.Printf(" ❔ Question #%d: %s = ", questionNumber, pair.Question)
 	fmt.Scanf("%d", &UserAnswer)
 	if UserAnswer == pair.Answer {
 		return true
@@ -89,8 +110,36 @@ func checkForCSVEnding(csvFile string) bool {
 	return false
 }
 
+/*
+doesFileExist will return true in case a given file exists
+if not it will return false
+*/
+func doesFileExist(path string) bool {
+	state, err := os.Stat(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+		fmt.Println(state)
+	}
+	return true
+}
+
 func main() {
-	CVSFile := flag.String("csv", "problems.csv", "Enter as a Parameter the CSV-File")
+	CVSFile := flag.String("csv", "problems.csv", "Enter a the CSV-File with Questions like 5+5 = 10")
+	TimeOut := flag.Int("timeout", 7000, "Enter the time every player get for a question")
+	flag.Parse()
+	if !doesFileExist(*CVSFile) {
+		fmt.Printf("[-] An error occured while trying to read the File: %s \n", "File does not exist")
+		os.Exit(0)
+	}
+	_Game := Game{
+		CorrectAnswers: 0,
+		TimeOutAfter:   *TimeOut,
+	}
+	fmt.Println("------------------------------------------")
+	fmt.Printf(" 🎮 Gameconfig 🎮 : Timeout = %d Sec. ⏱️ \n", (_Game.TimeOutAfter / 1000))
+	fmt.Println("------------------------------------------")
 	flag.Parse()
 	if !checkForCSVEnding(*CVSFile) {
 		*CVSFile = (*CVSFile) + ".csv"
@@ -100,13 +149,15 @@ func main() {
 		os.Exit(1)
 	} else {
 		for number, pair := range readCSV(filePointer) {
-			if askAQuestion(number, pair) {
-				fmt.Println(">> Correct Answer")
+			if askAQuestion(number+1, pair, _Game.TimeOutAfter) {
+				fmt.Println(">> Correct Answer 👍")
 			} else {
-				fmt.Println(">> Wrong Answer")
+				fmt.Println(">> Wrong Answer 👎")
+				fmt.Println("\n>> Boy oh boy 💩 💩")
 				os.Exit(0)
 			}
 		}
+		fmt.Println("\n>> Good job you completed the game! 👍 👍")
 	}
 	os.Exit(0)
 }
